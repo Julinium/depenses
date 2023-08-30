10304b3e6ff75826ecde64e550949d409235e629
Julinium <121466317+Julinium@users.noreply.github.com>
2023-08-30 17:57:01 +0100

    Update models.py

--- depenses/base/models.py
+++ depenses/base/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Access(models.Model):
+    
+    active = models.BooleanField(default = True)
+    date = models.DateTimeField(default = timezone.now)
+    title = models.CharField(max_length = 256)
+    reason = models.CharField(max_length = 256)
+    location = models.CharField(max_length = 256)
+    details = models.TextField(blank = True)
+    archived = models.BooleanField(default = False)
+    created_on = models.DateTimeField(auto_now_add = True)
+    updated_on = models.DateTimeField(auto_now = True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+	
+    def __str__(self):
+        return str(self.title) + " - " + self.date.strftime("%a %d %b %Y, %H:%M")
+
+    class Meta():
+        ordering = ['-date', '-updated_on']
