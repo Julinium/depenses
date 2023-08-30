79a6632d9a7ba6f6f2fa6dbe83dc7b3104d733ec
Julinium <julinium@gmail.com>
2023-08-30 17:51:41 +0100

    Started routing

--- depenses/base/views.py
+++ depenses/base/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    context = {'message':'This message was passed from the View...'}
+    return render(request, 'base/home.html', context)
--- depenses/depenses/urls.py
+++ depenses/depenses/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('base.urls')),
+    # path('expenses', include('expenses.urls')),
+    # path('incomes', include('incomes.urls')),
+    # path('transfers', include('transfers.urls')),
 ]
